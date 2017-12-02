c969bd539d3aa44876ac41d21d1015b1dc3c04c9
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2017-12-02 15:59:49 +0300

    script help

--- t_script.sh
+++ t_script.sh
@@ -6,6 +6,9 @@ TRACKER_HOST=project
 if [ $# -gt 0 ]; then
   if [ "$1" == "-help" ]; then
     echo 'управление серверами с нодой'
+    echo 't_script.sh tracker -start'
+    echo 't_script.sh tracker -stop'
+    echo 't_script.sh tracker -status'
   fi
   if [ "$1" == "-list" ]; then
     echo "$(ps ax | grep node)"
@@ -14,8 +17,6 @@ if [ $# -gt 0 ]; then
     re='^[0-9]+$'
     pid="$(pgrep -f serverNode | awk '{ print $1 }')"
     if [ "$2" == "-start" ]; then
-      #if [ -z ${pid+x} ]; then
-      #if [ "$#pid" -gt 0 ]; then
       if ! [[ $pid =~ $re ]] ; then
         echo starting node server
         cd $HOME_PATH/$TRACKER_HOST
@@ -26,16 +27,14 @@ if [ $# -gt 0 ]; then
       fi
     fi
     if [ "$2" == "-stop" ]; then
-      #if [ -z ${pid+x} ]; then
       if ! [[ $pid =~ $re ]] ; then
         echo this server does not exists
       else
-        echo this server exists -- kill him 
+        echo this server exists -- kill him
         kill $pid
       fi
     fi
     if [ "$2" == "-status" ]; then
-      #if [ -z ${pid+x} ]; then
       if ! [[ $pid =~ $re ]] ; then
         echo this server does not exists
       else
@@ -46,4 +45,5 @@ if [ $# -gt 0 ]; then
   fi
 else
     echo "No parameters"
+    echo "Try t_script.sh -help"
 fi
