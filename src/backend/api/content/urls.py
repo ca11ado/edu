d59137d73ecdf1ced7dd5ef7dbdc75b2580fcff5
Roo <ra@tceh.com>
2017-12-18 20:03:21 +0300

    get cubic api working, test cubic added

--- src/backend/api/content/urls.py
+++ src/backend/api/content/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('cubic/<int:cubic_id>/', views.get_cubic, name='cubic'),
 ]
--- src/backend/api/content/views.py
+++ src/backend/api/content/views.py
@@ -1,5 +1,12 @@
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
-def index(request):
-    response = JsonResponse({'foo': 'bar'})
-    return response
+from .models import Cubic
+
+def get_cubic(request, cubic_id):
+    cubic = get_object_or_404(Cubic, pk=cubic_id)
+    response = {}
+    response['id'] = cubic.id
+    response['name'] = cubic.name
+    response['content'] = cubic.content
+    return JsonResponse(response)
