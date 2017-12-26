2e8b7ff292686989154b8ef564204f081db8b0ab
Roo <ra@tceh.com>
2017-12-26 11:25:54 +0300

    trailing slash fix

--- backend/api/content/urls.py
+++ backend/api/content/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cubic/<int:cubic_id>/', views.get_cubic, name='cubic'),
+    path('cubic/<int:cubic_id>', views.get_cubic, name='cubic'),
 ]
