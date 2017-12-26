6239813cefca9a61a71812a85f2267eaf94de695
Roo <ra@tceh.com>
2017-12-26 13:01:33 +0300

    /get/cubics method added

--- backend/api/content/urls.py
+++ backend/api/content/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('cubic/<int:cubic_id>', views.get_cubic, name='cubic'),
+    path('cubics', views.get_all_cubics, name='all_cubics'),
 ]
--- backend/api/content/views.py
+++ backend/api/content/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 
 from .models import Cubic
 
+
 def get_cubic(request, cubic_id):
     cubic = get_object_or_404(Cubic, pk=cubic_id)
     response = {}
@@ -10,3 +11,11 @@ def get_cubic(request, cubic_id):
     response['name'] = cubic.name
     response['content'] = cubic.content
     return JsonResponse(response)
+
+
+def get_all_cubics(request):
+    all_cubics = Cubic.objects.all()
+    response = []
+    for cubic in all_cubics:
+        response.append({'id': cubic.id, 'name': cubic.name})
+    return JsonResponse(response, safe=False)
