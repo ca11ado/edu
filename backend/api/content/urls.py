47eec534b7d1f0abe1b291175bc563ba7241fc72
Roo <ra@tceh.com>
2017-12-27 14:18:58 +0300

    /get/tag/<id> and /get/tags api methods added

--- backend/api/content/urls.py
+++ backend/api/content/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('cubic/<int:cubic_id>', views.get_cubic, name='cubic'),
     path('cubics', views.get_all_cubics, name='all_cubics'),
+    path('tag/<int:tag_id>', views.get_tag, name='tag'),
+    path('tags', views.get_all_tags, name='all_tags'),
 ]
--- backend/api/content/views.py
+++ backend/api/content/views.py
@@ -1,15 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
-from .models import Cubic
+from .models import Cubic, Tag
 
 
 def get_cubic(request, cubic_id):
     cubic = get_object_or_404(Cubic, pk=cubic_id)
-    response = {}
-    response['id'] = cubic.id
-    response['name'] = cubic.name
-    response['content'] = cubic.content
+    response = {'id': cubic.id, 'name': cubic.name, 'content': cubic.content}
     return JsonResponse(response)
 
 
@@ -18,4 +15,21 @@ def get_all_cubics(request):
     response = []
     for cubic in all_cubics:
         response.append({'id': cubic.id, 'name': cubic.name})
+    return JsonResponse(response, safe=False)
+
+
+def get_tag(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    cubics = []
+    for cubic in tag.cubics.all():
+        cubics.append(cubic.id)
+    response = {'id': tag.id, 'name': tag.name, 'cubics': cubics}
+    return JsonResponse(response)
+
+
+def get_all_tags(request):
+    all_tags = Tag.objects.all()
+    response = []
+    for tag in all_tags:
+        response.append({'id': tag.id, 'name': tag.name})
     return JsonResponse(response, safe=False)
