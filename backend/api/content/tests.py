0ffbd998562c929177d3f968e9bd3ff7b7fda149
Roo <ra@tceh.com>
2017-12-27 15:30:04 +0300

    cubic and tag tests

--- backend/api/content/tests.py
+++ backend/api/content/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.test.client import Client
+from .models import Cubic, Tag
 
-# Create your tests here.
+
+class CubicsTestCase(TestCase):
+    def create_cubic(self):
+        cubic = Cubic()
+        cubic.name = 'Тестовый кубик'
+        cubic.content = '123'
+        cubic.save()
+        return cubic
+
+    def create_tag(self, cubic):
+        tag = Tag()
+        tag.name = 'Тестовый тег'
+        tag.save()
+        tag.cubics.add(cubic)
+        tag.save()
+
+    @classmethod
+    def setUpClass(cls):
+        test = CubicsTestCase()
+        cubic = CubicsTestCase.create_cubic(test)
+        CubicsTestCase.create_tag(test, cubic)
+        super().setUpClass()
+
+    def test_get_single_cubic(self):
+        c = Client()
+        response = c.get('/get/cubic/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_all_cubics(self):
+        c = Client()
+        response = c.get('/get/cubics')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_single_tag(self):
+        c = Client()
+        response = c.get('/get/tag/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_all_tags(self):
+        c = Client()
+        response = c.get('/get/tags')
+        self.assertEqual(response.status_code, 200)
+
