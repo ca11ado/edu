87ffe3274c45d8bfae7e61eb8217fae8c6bcd816
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2018-01-01 18:30:20 +0300

    Access-Control-Allow-Origin add all

--- frontend/cors-middleware.js
+++ frontend/cors-middleware.js
@@ -2,7 +2,8 @@
 module.exports = function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3008');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3008');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
